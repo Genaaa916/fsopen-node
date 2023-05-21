86e4145930bca3fd2a21f53d7a5c16073b24434f
Gena <genci.behxheti@gmail.com>
2023-05-21 20:18:21 +0300

    -

--- mongoose.js
+++ mongoose.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 //i already tried the password through the argv and it worked, now it's already set to use the .env file
 dotenv.config({ path: "./.env" });
 
-const password = process.env.PASSWORD;
+const password = process.env.MONGODB_URI;
 
-const url = `mongodb+srv://Genci:${password}@cluster1.qmk7n43.mongodb.net/?retryWrites=true&w=majority`;
+const url = `${process.env.MONGODB_URI}?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
